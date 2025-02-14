b7002b0038332eff2277e8c3af01528b60adb764
Jarred Villegas <jarredvillegas2@gmail.com>
2025-02-14 20:19:22 +0800

    Deployed in hosting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
